2632737c2a7f71332453aae7350cdcd904861e8e
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2021-10-22 15:27:01 +0200

    start af pogram

--- .idea/Main.py
+++ .idea/Main.py
@@ -16,8 +16,9 @@ screen = pygame.display.set_mode([500, 500])
 running = True
 while running:
 
-    screen.fill("white")
+    screen.fill((255,255,255))
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
