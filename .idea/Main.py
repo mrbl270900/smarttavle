d5053ffaeca3a6286055b729464b6b2a48b99962
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2021-10-25 12:48:35 +0200

    start af pogram

--- .idea/Main.py
+++ .idea/Main.py
@@ -5,33 +5,39 @@ from urllib.request import urlopen
 import pygame
 from datetime import datetime
 
-
-joke = ""
-response = urlopen("https://v2.jokeapi.dev/joke/Any")
-data_joke = json.loads(response.read())
-
-url = urllib.request.urlopen("https://www.metaweather.com/api/location/554890/")  #weather in københavn
-output = url.read().decode('utf-8')
-raw_api_dict = json.loads(output)
-url.close()
-data_weather = raw_api_dict
-
-if "joke" in data_joke:
-    joke = data_joke["joke"]
-else:
-    joke = data_joke["setup"]
-    joke = joke + " " + data_joke["delivery"]
-while(len(joke)>100):
-    response = urlopen("https://v2.jokeapi.dev/joke/Any")
-    data_joke = json.loads(response.read())
+def chekJoke(data_joke):
     if "joke" in data_joke:
         joke = data_joke["joke"]
-    else:
+    else: # else put botehe the setup an delivery in the joke varibul
         joke = data_joke["setup"]
         joke = joke + " " + data_joke["delivery"]
+        return joke
 
-weather = data_weather.get("consolidated_weather")[0].get("weather_state_name")
-temp = str(data_weather.get("consolidated_weather")[0].get("the_temp"))
+def getJoke():
+    response = urlopen("https://v2.jokeapi.dev/joke/Any")
+    data_joke = json.loads(response.read())
+    joke = ""
+    joke = chekJoke(data_joke)
+    while(len(joke)>100):
+        response = urlopen("https://v2.jokeapi.dev/joke/Any")
+        data_joke = json.loads(response.read())
+        chekJoke(data_joke)
+    return joke
+
+def getTime():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+def getWeather():
+    url = urllib.request.urlopen("https://www.metaweather.com/api/location/554890/")  #weather in københavn
+    output = url.read().decode('utf-8')
+    raw_api_dict = json.loads(output)
+    url.close()
+    data_weather = raw_api_dict
+    weather = data_weather.get("consolidated_weather")[0].get("weather_state_name")
+    temp = str(data_weather.get("consolidated_weather")[0].get("the_temp"))
+    return weather + " and " + temp + " degrees out side";
 
 pygame.init()
 pygame.font.init()
@@ -40,49 +46,22 @@ timefont = pygame.font.SysFont('Helvetica', 50)
 
 
 joketext = myfont.render("Todays joke:", False, (0, 0, 0))
-thejoke = myfont.render(joke, False, (0, 0, 0))
+thejoke = myfont.render(getJoke(), False, (0, 0, 0))
 
 weathertext = myfont.render("Weather in Copenhagen", False, (0, 0, 0))
-theweather = myfont.render(weather + " and " + temp + " degrees out side", False, (0, 0, 0))
+theweather = myfont.render(getWeather(), False, (0, 0, 0))
 
 screen = pygame.display.set_mode([800, 200])
 running = True
 while running:
+    time = timefont.render(getTime(), False, (0,0,0))
 
-
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    time = timefont.render(current_time, False, (0,0,0))
-
-    if(current_time == "23:59:59"):
-        response = urlopen("https://v2.jokeapi.dev/joke/Any")
-        data_joke = json.loads(response.read())
-        if "joke" in data_joke:
-            joke = data_joke["joke"]
-        else:
-            joke = data_joke["setup"]
-            joke = joke + " " + data_joke["delivery"]
-        while(len(joke)>100):
-            response = urlopen("https://v2.jokeapi.dev/joke/Any")
-            data_joke = json.loads(response.read())
-            if "joke" in data_joke:
-                joke = data_joke["joke"]
-            else:
-                joke = data_joke["setup"]
-                joke = joke + " " + data_joke["delivery"]
-        thejoke = myfont.render(joke, False, (0, 0, 0))
+    if(getTime() == "23:59:59"):
+        thejoke = myfont.render(getJoke(), False, (0, 0, 0))
 
     for x in range(24):
-        if(current_time == str(x) + ":20:00"):
-            url = urllib.request.urlopen("https://www.metaweather.com/api/location/554890/")  #weather in københavn
-            output = url.read().decode('utf-8')
-            raw_api_dict = json.loads(output)
-            url.close()
-            data_weather = raw_api_dict
-            weather = data_weather.get("consolidated_weather")[0].get("weather_state_name")
-            temp = str(data_weather.get("consolidated_weather")[0].get("the_temp"))
-            theweather = myfont.render(weather + " and " + temp + " degrees out side", False, (0, 0, 0))
-
+        if(getTime() == str(x) + ":00:00"):
+            theweather = myfont.render(getWeather(), False, (0, 0, 0))
 
     screen.fill((255,255,255))
 
